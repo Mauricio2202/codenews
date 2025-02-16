039e01cee942997106bfb0d708a9119a4cb723cb
Omega <elbackenddev22@gmail.com>
2025-02-15 23:20:59 -0600

    Añadiendo menú desplegable

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,24 +1,52 @@
+import React, { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import menuIcon from '../assets/menu.png';
+import '../components/Layout.css';
 import { Home } from './Home';
 import { Clases } from './Clases';
 
 const Layout = () => {
-    
-return (
-    <div>
-    <nav>
-        <ul>
-            <li><Link to="/">Inicio</Link></li>
-            <li><Link to="news">Noticias</Link></li>
-            <li><Link to="contribuyente">Contribuyente</Link></li>
-        </ul>
-    </nav>
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    <Outlet /> 
-    <Home />
-    <Clases />
-    </div>
-);
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
+
+    return (
+        <div>
+            <nav>
+                <input
+                    type="checkbox"
+                    id="menu-toggle"
+                    className="menu-checkbox"
+                    checked={isMenuOpen} 
+                    onChange={toggleMenu}
+                />
+                <label htmlFor="menu-toggle" className="menu-icon">
+                    <img src={menuIcon} alt="Menú" />
+                </label>
+                <ul className={`menu-list ${isMenuOpen ? 'open' : ''}`}>
+                    <li>
+                        <Link to="/" onClick={closeMenu}>Inicio</Link>
+                    </li>
+                    <li>
+                        <Link to="news" onClick={closeMenu}>Noticias</Link>
+                    </li>
+                    <li>
+                        <Link to="contribuyente" onClick={closeMenu}>Contribuyente</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <Outlet />
+            <Home />
+            <Clases />
+        </div>
+    );
 };
 
 export default Layout;
